c2c25eace3ba4727eb484397347a253219cf062a
Raul Cabrero Rouco <rcabrero@c1r13s2.42madrid.com>
2024-08-31 13:53:55 +0200

    Cambios tontos

--- philo/philo.h
+++ philo/philo.h
@@ -20,6 +20,27 @@
 
 # define PHILO_MAX 201
 
+//TODO: Esta estructura es pprovisional...
+typedef struct s_philo
+{
+	int is_eaten;
+	int	is_sleep;
+	int	is_dead;
+}			t_philo;
+
+//Informacion relevante sobre la ejecucion del programa
+typedef struct s_philo_info
+{
+    int             num_philos;       // Número de filósofos, el numero de filosofos siempre es el mismo que el numero de tenedores!!
+    t_philo			*philos;
+	pthread_mutex_t *forks;           // Array de mutexes para los tenedores
+    int             time_to_die;      // Tiempo en morir (en milisegundos)
+    int             time_to_eat;      // Tiempo en comer (en milisegundos)
+	int				time_to_sleep;	  // Tiempo en dormir (en milisegundos)
+    int             max_meals;        // Veces máximas que cada filósofo debe comer
+    int             is_dead;		  // Controla si un filosoof ha muerto para parar la ejecucion
+}               t_philo_info;
+
 //UTILS Y CHECK_ERRORS
 int check_args(int argc,char **argv);
 int	check_arg_content(char *arg);
