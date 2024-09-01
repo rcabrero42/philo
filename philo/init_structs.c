fac9e584f8c9db29d5da56f0557b94a50f5ae2bc
Raul Cabrero Rouco <rcabrero@c1r5s1.42madrid.com>
2024-09-01 21:15:15 +0200

    NUevo atoi e inicio de estructura y control de parametros

--- philo/init_structs.c
+++ philo/init_structs.c
@@ -16,12 +16,21 @@
  * Inicializa la estructura de informacion
  *	Hacemmos el atoi de todos y convertimos lo qeu necesitemos.
  */
-int init_info(char **argv)
+int init_info(char **argv, int has_max_eat)
 {
 	t_philo_info p_info;
 
-	//Numero de filosofos...
-	
+	p_info.num_philos = ft_atoi(argv[0]);
+	p_info.time_to_die = ft_atoi(argv[1]);
+	p_info.time_to_eat = ft_atoi(argv[2]);
+	p_info.time_to_sleep = ft_atoi(argv[3]);
+	if(has_max_eat)
+	{
+		p_info.max_meals = ft_atoi(argv[4]);
+		if(p_info.max_meals != -1)
+			return (-1);
+	}
+	if(p_info.num_philos != -1 || p_info.time_to_die != -1 
+		|| p_info.time_to_eat != -1	|| p_info.time_to_sleep != -1)
+		return (-1);
 }
-
-
